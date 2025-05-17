3f39b8f157ae3283b4e52a460143998b3544c552
MohdDaniish <danish.ekaasha@gmail.com>
2025-05-17 17:04:09 +0530

    adad

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,44 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "@rainbow-me/rainbowkit/styles.css";
-import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { connectorsForWallets, getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { WagmiProvider } from "wagmi";
 import { bsc, bscTestnet, opBNBTestnet, polygon } from "wagmi/chains";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import { store } from "./Redux/Store.js";
 import { Provider } from "react-redux";
+import {
+  binanceWallet,
+  metaMaskWallet,
+  tokenPocketWallet,
+  trustWallet,
+  walletConnectWallet,
+} from '@rainbow-me/rainbowkit/wallets';
+
+
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: 'Recommended',
+      wallets: [trustWallet, metaMaskWallet,tokenPocketWallet, binanceWallet, walletConnectWallet],
+    },
+  ],
+  {
+    appName: 'My RainbowKit App',
+    projectId: 'YOUR_PROJECT_ID',
+  }
+);
 
 // import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 export const config = getDefaultConfig({
+  connectors,
   appName: "My RainbowKit App",
   projectId: "9ead31f799e15b5963591c2b014b64bd",
   chains: [polygon],
   ssr: true,
 });
+
 
 const queryClient = new QueryClient();
 
